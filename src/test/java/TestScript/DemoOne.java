6c6969b867a78274ac18f60d8ccba66ae5399a50
lilan31 <sahoo.hrudaya@gmail.com>
2021-01-11 18:44:49 +0530

    3rd commit--add resouses Folder and add browser setup in testscript class

--- src/test/java/TestScript/DemoOne.java
+++ src/test/java/TestScript/DemoOne.java
@@ -1,6 +1,8 @@
 package TestScript;
 
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import genericLib.BaseClass;
@@ -9,6 +11,12 @@ public class DemoOne extends BaseClass {
 
 	@Test
 	public void testCaseOne(){
+		
 		System.out.println("testcase one");
+		System.setProperty("webdriver.chrome.driver", "F:\\APTTUS_INTERVIEW\\GithubPractice\\Resourses\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://facebook.com");
+		System.out.println("testscript completed");
 	}
 }
